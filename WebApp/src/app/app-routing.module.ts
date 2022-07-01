7be7d0bdb3c474a01f36fc53b6553efd337946fd
Egor Denysenko <egor.denys0@gmail.com>
2022-07-01 22:23:45 +0200

    Implemented Definitive Routing

--- WebApp/src/app/app-routing.module.ts
+++ WebApp/src/app/app-routing.module.ts
@@ -1,53 +1,36 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-// layouts
-import { AdminComponent } from "./layouts/admin/admin.component";
-import { AuthComponent } from "./layouts/auth/auth.component";
-
-// admin views
-import { DashboardComponent } from "./views/admin/dashboard/dashboard.component";
-import { MapsComponent } from "./views/admin/maps/maps.component";
-import { SettingsComponent } from "./views/admin/settings/settings.component";
-import { TablesComponent } from "./views/admin/tables/tables.component";
-
-// auth views
-import { LoginComponent } from "./views/auth/login/login.component";
-import { RegisterComponent } from "./views/auth/register/register.component";
-
-// no layouts views
-import { IndexComponent } from "./views/index/index.component";
-import { LandingComponent } from "./views/landing/landing.component";
-import { ProfileComponent } from "./views/profile/profile.component";
 
 const routes: Routes = [
+  {
+    path:'dashboard',
+    component: ,
+    children: [
+      { path: "Dashboard", component: },
+      { path: "TraindData", component:},
+      { path: "tables", component: },
+      { path: "", redirectTo: "dashboard", pathMatch: "full" },
+    ],
+  }
   // admin views
   {
     path: "admin",
-    component: AdminComponent,
+    component: ,
     children: [
-      { path: "dashboard", component: DashboardComponent },
-      { path: "settings", component: SettingsComponent },
-      { path: "tables", component: TablesComponent },
-      { path: "maps", component: MapsComponent },
-      { path: "", redirectTo: "dashboard", pathMatch: "full" },
+      { path: "TrainCommand", component:  },
     ],
   },
   // auth views
   {
     path: "auth",
-    component: AuthComponent,
+    component: ,
     children: [
-      { path: "login", component: LoginComponent },
-      { path: "register", component: RegisterComponent },
+      { path: "login", component:},
+      { path: "register", component:},
       { path: "", redirectTo: "login", pathMatch: "full" },
     ],
   },
-  // no layout views
-  { path: "profile", component: ProfileComponent },
-  { path: "landing", component: LandingComponent },
-  { path: "", component: IndexComponent },
-  { path: "**", redirectTo: "", pathMatch: "full" },
 ];
 
 @NgModule({
