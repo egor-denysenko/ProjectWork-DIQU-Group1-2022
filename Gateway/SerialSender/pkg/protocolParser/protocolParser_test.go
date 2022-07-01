923b0e753d7acd8e348f53e902e990f5012d4ac6
Egor Denysenko <egor.denys0@gmail.com>
2022-07-01 15:27:24 +0200

    Light And Temperature Suit Test Functioning

--- Gateway/SerialSender/pkg/protocolParser/protocolParser_test.go
+++ Gateway/SerialSender/pkg/protocolParser/protocolParser_test.go
@@ -1,20 +1,41 @@
 package protocolParser
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
-func TestDetermineCommand(t *testing.T) {
-	recieverTestCases := []struct {
+func TestCreateSetTempMess(t *testing.T) {
+	receiverTestCases := []struct {
 		name     string
-		mockData byte
-		want     error
+		mockData map[string]uint8
+		want     []byte
 	}{
-		{name: "Verify Correct Parsing Into Enum", mockData: 69, want: nil},
-		{name: "Return Error Because The Value Is Not In The Enum", mockData: 33, want: WrongGatewayCommand},
+		{name: "Verify Correct Json Creation", mockData: map[string]uint8{"WagonCommand": 70, "TargetWagon": 22, "TargetTemperature": 11}, want: []byte{22, 70, 11}},
+		{name: "Correct Error Management", mockData: map[string]uint8{"WagonCommand": 70, "TargetWagon": 22, "TargetTemperatureee": 11}, want: nil},
 	}
-	for _, testCase := range recieverTestCases {
+	for _, testCase := range receiverTestCases {
 		t.Run(testCase.name, func(t *testing.T) {
-			got, _ := determineCommand(testCase.mockData)
-			if got != testCase.want {
+			got := createSetTempMess(testCase.mockData)
+			if bytes.Compare(got, testCase.want) != 0 {
+				t.Errorf("Running Test %v: \n Expected %v want %v", testCase.name, got, testCase.want)
+			}
+		})
+	}
+}
+func TestCreateSetLightMess(t *testing.T) {
+	receiverTestCases := []struct {
+		name     string
+		mockData map[string]uint8
+		want     []byte
+	}{
+		{name: "Verify Correct Json Creation", mockData: map[string]uint8{"WagonCommand": 70, "TargetWagon": 22, "TargetLight": 11}, want: []byte{22, 70, 11}},
+		{name: "Correct Error Management", mockData: map[string]uint8{"WagonCommand": 70, "TargetWagon": 22, "TargetLighteee": 11}, want: nil},
+	}
+	for _, testCase := range receiverTestCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			got := createSetLightMess(testCase.mockData)
+			if bytes.Compare(got, testCase.want) != 0 {
 				t.Errorf("Running Test %v: \n Expected %v want %v", testCase.name, got, testCase.want)
 			}
 		})
